a74150bd70ea1a07db06162afe2f8d2e722823e4
Kaan YASSIBAŞ <kyassibas@gmail.com>
2025-12-11 00:23:34 +0300

    Update auth & task service configs, requirements and docker-compose

--- auth-service/app/config.py
+++ auth-service/app/config.py
@@ -11,7 +11,11 @@ class Settings:
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
     ACCESS_EXPIRE_HOURS: int = int(os.getenv("ACCESS_EXPIRE_HOURS", "4"))
 
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "mysql+pymysql://todo_user:todo_pass@db:3306/todo_db")
+    DATABASE_URL: str = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://todo_user:todo_pass@postgres-host:5432/todo_db",
+)
+
 
     CORS_ORIGINS: list[str] = os.getenv(
         "CORS_ORIGINS",
--- task-service/app/config.py
+++ task-service/app/config.py
@@ -9,7 +9,11 @@ class Settings:
 
     JWT_SECRET: str = os.getenv("JWT_SECRET", "devsecret")
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "mysql+pymysql://todo_user:todo_pass@db:3306/todo_db")
+    DATABASE_URL: str = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://todo_user:todo_pass@postgres-host:5432/todo_db",
+)
+
 
     CORS_ORIGINS: list[str] = os.getenv(
         "CORS_ORIGINS",
