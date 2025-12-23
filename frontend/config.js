c9efef6dea39cd090c1a92153e41f01aa3c4e88e
adaezel <ezeladayalcin@gmail.com>
2025-12-23 18:53:04 +0300

    Update config.js and requriements for seperate branches

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_URL: "https://auth-service-kyassibas-dev.apps.rm2.thpm.p1.openshiftapps.com",
-  TASK_URL: "https://task-service-kyassibas-dev.apps.rm2.thpm.p1.openshiftapps.com",
+  AUTH_URL: "https://auth-service-adaezel-dev.apps.rm2.thpm.p1.openshiftapps.com",
+  TASK_URL: "https://task-service-adaezel-dev.apps.rm2.thpm.p1.openshiftapps.com",
 };
+
