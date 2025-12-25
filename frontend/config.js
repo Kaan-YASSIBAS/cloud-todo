df8ee269afe827f29004ab7061e90868589e3fc8
Kaan YASSIBAŞ <kyassibas@gmail.com>
2025-12-26 00:36:02 +0300

    test: frontend deploy

--- frontend/config.js
+++ frontend/config.js
@@ -2,3 +2,5 @@ window.APP_CONFIG = {
   AUTH_URL: "https://auth-service-kyassibas-dev.apps.rm2.thpm.p1.openshiftapps.com",
   TASK_URL: "https://task-service-kyassibas-dev.apps.rm2.thpm.p1.openshiftapps.com",
 };
+
+// ci-cd test
